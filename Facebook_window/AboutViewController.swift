2c1e18d57b4636e27a803e2aed020e519d4ce161
Kate Ulasik <>
2021-08-17 12:28:15 +0200

    first commit

---
+++ Facebook_window/AboutViewController.swift
@@ -0,0 +1,34 @@
+//
+//  AboutViewController.swift
+//  Facebook_window
+//
+//  Created by Katerina Ulasik on 15.08.2021.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+    
+    @IBAction func closeLogIn(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
